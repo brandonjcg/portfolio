57f170e483c8d6fecf28d98327af995a112b1c00
Brandon Castillo <brajoel98@gmail.com>
2025-03-19 20:18:32 -0700

    feat(app): :sparkles: first edition of portfolio

--- src/app/components/ui/Navbar.tsx
+++ src/app/components/ui/Navbar.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link';
 const navLinks = [
   { href: '#about', label: 'About' },
   { href: '#projects', label: 'Projects' },
-  { href: '#contact', label: 'Contact' },
 ];
 
 export const Navbar = () => {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,21 @@ import { List } from './components/List/List';
 import { experiencesRows } from './data';
 
 export default function Home() {
+  const beginDate = new Date('2019-07-24');
+  const endDate = new Date();
+  const years = endDate.getFullYear() - beginDate.getFullYear();
+
   return (
     <>
-      <h1 className="text-4xl text-center">🚧 Portfolio in progress 🚧</h1>
       <div className="space-y-10">
-        <section id="about">
+        <section id="about" className="scroll-mt-16">
+          <h1 className="text-4xl text-center">{`I'm Brandon Castillo 👨🏽‍💻`}</h1>
           <p className="mt-3 text-gray-400 leading-relaxed">
-            Los tacos al pastor, con su piña bien dorada, eran la inspiración
-            del poeta. Ándale pues, que en este país del chile y el maíz, la
-            vida siempre da vueltas como los molcajetes.
+            {`I'm a full-stack developer with ${years} years of experience in IT. I have been working with technologies like React, Node.js, NoSQL, and SQL databases. I have a strong background in database design, data analysis, and system optimization. I'm passionate about learning new technologies and building scalable and maintainable software.`}
           </p>
         </section>
-        <section id="experience">
+        <section id="projects" className="scroll-mt-16">
           <List title="Experience" rows={experiencesRows} />
-        </section>
-        <section id="contact">
-          <List title="Contact" rows={experiencesRows} />
         </section>
       </div>
     </>
